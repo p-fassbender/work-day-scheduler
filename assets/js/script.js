4f1d5fd8c873a2b8efc1ef9d93b50cae49c97346
Preston Fassbender <fassbenderp0551@gmail.com>
2022-01-09 11:56:38 -0600

    created time blocks in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
-var currentTime = moment().format("MMMM Do YYYY, h:mm:ss a")
+var currentTime;
 
 // displays and updates the displayed time every second
 function update() {
+    currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
     $("#currentDay").text(currentTime);
 }
 setInterval(update, 1000);
