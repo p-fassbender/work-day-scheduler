1d432a6953ac886ee742c37b576058292ff14d68
Preston Fassbender <fassbenderp0551@gmail.com>
2022-01-09 15:58:11 -0600

    scheduled events save and load from localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var currentTime;
+var eventObjArray = ["","","","","","","","",""];
 
 function update() {
     // gets the current moment time and adds it to the currentDay paragraph to display on the page
@@ -26,3 +27,42 @@ function update() {
     });
 }
 setInterval(update, 1000);
+
+// sets the text of the description text area to be the entered text upon clicking off of textarea
+$(".description").on("blur", function () {
+    var text = $(this).val().trim();
+    $(this).text(text);
+})
+
+// saves the individual schedule event text to an array and saves that array to localstorage
+function saveEvent(textArea) {
+    var eventObj = {
+        eventText: textArea.text(),
+        index: parseInt(textArea.attr("id"))
+    }
+     eventObjArray[eventObj.index] = eventObj;
+     localStorage.setItem("scheduled-events", JSON.stringify(eventObjArray));
+}
+
+// upon clicking save button calls saveEvent function
+$(".saveBtn").on("click", function () {
+    saveEvent($(this).siblings("textarea"));
+});
+
+// loads saved schedule events from localstorage and puts them in an array to be displayed
+function loadSchedule() {
+    var savedEvents = JSON.parse(localStorage.getItem("scheduled-events"))
+    //eventObjArray = savedEvents;
+    for (let i = 0; i < savedEvents.length; i++) {
+        eventObjArray[i] = savedEvents[i];
+    }
+    displaySchedule(eventObjArray);
+}
+
+function displaySchedule() {
+    for (let i = 0; i < eventObjArray.length; i++) {
+        $("#"+ i).text(eventObjArray[i].eventText);
+    }
+}
+
+loadSchedule();
