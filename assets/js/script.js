88e7b9ea3ce1cabc216662bd869a26cd3b9c7c94
Preston Fassbender <fassbenderp0551@gmail.com>
2022-01-09 13:01:54 -0600

    added check/update for the timeblock colors based on current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,29 @@
 var currentTime;
 
-// displays and updates the displayed time every second
+
 function update() {
+    // gets the current moment time and adds it to the currentDay paragraph to display on the page
     currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
     $("#currentDay").text(currentTime);
+
+    // for each time block checks the hour and compares it to the currentTime and changes the class name accordingly
+    $(".hour").each(function () {
+        var time = moment($(this).text(), "Ha");
+        if (moment().isBefore(time)) {
+            $(this).siblings(".description").removeClass("past");
+            $(this).siblings(".description").removeClass("present");
+            $(this).siblings(".description").addClass("future");
+        }
+        else if (Math.abs(moment().diff(time, "hours")) < 1) {
+            $(this).siblings(".description").removeClass("past");
+            $(this).siblings(".description").removeClass("future");
+            $(this).siblings(".description").addClass("present");
+        }
+        else {
+            $(this).siblings(".description").removeClass("present");
+            $(this).siblings(".description").removeClass("future");
+            $(this).siblings(".description").addClass("past");
+        }
+    });
 }
 setInterval(update, 1000);
